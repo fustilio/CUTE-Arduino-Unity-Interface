656590d21e5cd62d82935b38703702a37ba6ca87
Francis Lee <francis@u.nus.edu>
2021-02-17 11:19:51 +0800

    Add instructional comments to examples

--- CUTE-AUI/examples/Hardware/Hardware.ino
+++ CUTE-AUI/examples/Hardware/Hardware.ino
@@ -1,21 +1,26 @@
-#include <CUTE_AUI.h>
-#include "DemoHardware.h"
+/*************************** HEADERS *******************************/
+#include <CUTE_AUI.h> // Library Header file
+#include "DemoHardware.h" // Project Header file
 
 /*************************** SETTINGS *******************************/
+// The rate that is agreed between devices accessing the serial port
 #define DATA_RATE 9600
-#define NUMBER_OF_COMMANDS 3
+// The maximum number of commands that the CUTE_AUI object can store
+#define NUMBER_OF_COMMANDS 4
 
-/*************************** VARIABLES *******************************/
+/*************************** VARIABLES ******************************/
 unsigned long currentMillis;
 CUTE_AUI ca;
 
-/*************************** COMMAND HOOKS ********************************************/
+/************************* COMMAND HOOKS ****************************/
+// The command strings that invokes the corresponding command
 #define COMMAND_STRING_PING "PING"
 #define COMMAND_STRING_DEMO_ON "DEMO_ON"
 #define COMMAND_STRING_DEMO_OFF "DEMO_OFF"
 #define COMMAND_STRING_DEMO_WRITE "WRITE"
 
-
+/*********************** COMMAND DEFINITIONS*************************/
+// Basic command to signal that a two-way connection has been established
 void CommandPing()
 {
   Serial.println("Hardware.ino: Ping");
@@ -33,32 +38,38 @@ void CommandDemoOff()
   DEMO_HARDWARE.SetActive(false);
 }
 
+// Command that writes to a internal variable of the demo hardware object
 void CommandWriteVal(int val)
 {
-  Serial.print("Demo hardware: Write val: ");
+  Serial.print("Demo hardware: Write value: ");
   Serial.println(val);
   DEMO_HARDWARE.WriteInternal(val);
 }
 
-/*************************** SETUP *******************************/
+/**************************** SETUP *********************************/
 void setup()
 {
   Serial.begin(DATA_RATE); // opens serial port, sets data rate to 9600 bps
+
   
-  ca.Init(&Serial, NUMBER_OF_COMMANDS);
+  ca.Init(&Serial, NUMBER_OF_COMMANDS); // Initialize the CUTE_AUI object
+  // Configure the command string and command to be invoked
   ca.AddCommand(COMMAND_STRING_PING, CommandPing);
   ca.AddCommand(COMMAND_STRING_DEMO_ON, CommandDemoOn);
   ca.AddCommand(COMMAND_STRING_DEMO_OFF, CommandDemoOff);
   ca.AddCommand(COMMAND_STRING_DEMO_WRITE, CommandWriteVal);
 
+  // Initialize demo hardware
   DEMO_HARDWARE.Init(ca);
 }
 
-/**************************** MAIN LOOP *******************************/
+/*************************** MAIN LOOP ********************************/
 void loop()
 {
+  // The CUTE_AUI object listens for commands on the serial port
   ca.WaitUnityCommand();
-
-  currentMillis = millis();
-  DEMO_HARDWARE.ReadInternal(currentMillis);
+  
+  currentMillis = millis(); // get current time elapsed
+  // invoke a demo function that reads an internal variable and prints to serial
+  DEMO_HARDWARE.ReadInternal(currentMillis); 
 }
--- CUTE-AUI/examples/Ping/Ping.ino
+++ CUTE-AUI/examples/Ping/Ping.ino
@@ -1,34 +1,44 @@
-#include <CUTE_AUI.h>
+/*************************** HEADERS *******************************/
+#include <CUTE_AUI.h> // Library Header file
 
 /*************************** SETTINGS *******************************/
+// The rate that is agreed between devices accessing the serial port 
 #define DATA_RATE 9600
-#define NUMBER_OF_COMMANDS 1
+// The maximum number of commands that the CUTE_AUI object can store
+#define NUMBER_OF_COMMANDS 1 
 
-/*************************** VARIABLES *******************************/
+/*************************** VARIABLES ******************************/
 CUTE_AUI ca;
 
-/*************************** COMMAND HOOKS ********************************************/
-#define COMMAND_STRING_PING "PING"
+/************************* COMMAND HOOKS ****************************/
+// The command string invokes the corresponding command
+#define COMMAND_STRING_PING "PING"  
 
+
+/*********************** COMMAND DEFINITIONS*************************/
+// Basic command to signal that a two-way connection has been established
 void CommandPing()
 {
   Serial.println("Ping");
 }
 
-/*************************** SETUP *******************************/
+/**************************** SETUP *********************************/
 void setup()
 {
-  Serial.begin(DATA_RATE); // opens serial port, sets data rate to 9600 bps
+  Serial.begin(DATA_RATE); // Opens serial port, sets data rate to 9600 bps
   Serial.setTimeout(1);
-  
-  ca.Init(&Serial, NUMBER_OF_COMMANDS);
+
+  ca.Init(&Serial, NUMBER_OF_COMMANDS);  // Initialize the CUTE_AUI object
+
+  // Configure the command string and command to be invoked
   ca.AddCommand(COMMAND_STRING_PING, CommandPing);
   
   delay(500);
 }
 
-/**************************** MAIN LOOP *******************************/
+/*************************** MAIN LOOP ********************************/
 void loop()
 {
+  // The CUTE_AUI object listens for commands on the serial port
   ca.WaitUnityCommand();
 }
